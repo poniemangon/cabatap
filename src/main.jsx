1df5395c2977b2194031d7ca8975b9cfda6ed108
gon_14 <poniemangon@gmail.com>
2026-07-20 21:55:00 -0300

    viewers

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './index.css'
 import App from './App.jsx'
@@ -7,5 +8,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
